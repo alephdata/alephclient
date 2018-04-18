5143546021a8418e29b890192e638bca92cf00eb
Friedrich Lindenberg <friedrich@pudo.org>
2018-04-18 07:45:44 +0200

    Bump version: 0.5.1 → 0.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='alephclient',
-    version='0.5.1',
+    version='0.5.2',
     description="Command-line client for Aleph API",
     author='Journalism Development Network',
     author_email='data@occrp.org',
