5739cc2e2c7cda46cd0e822c0aa98cffd0a17f3c
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-13 16:36:50 +0200

    Move YAML to util function

--- alephclient/tasks/bulkload.py
+++ alephclient/tasks/bulkload.py
@@ -1,39 +1,13 @@
-import os
-import yaml
+import logging
 
-from alephclient.tasks.util import load_collection
+from alephclient.tasks.util import load_collection, load_config_file
 
-
-def load_config_file(file_path):
-    """Load a YAML (or JSON) bulk load mapping file."""
-    file_path = os.path.abspath(file_path)
-    with open(file_path, 'r') as fh:
-        data = yaml.load(fh) or {}
-    return resolve_includes(file_path, data)
-
-
-def resolve_includes(file_path, data):
-    """Handle include statements in the graph configuration file.
-
-    This allows the YAML graph configuration to be broken into
-    multiple smaller fragments that are easier to maintain."""
-    if isinstance(data, (list, tuple, set)):
-        data = [resolve_includes(file_path, i) for i in data]
-    elif isinstance(data, dict):
-        include_paths = data.pop('include', [])
-        if not isinstance(include_paths, (list, tuple, set)):
-            include_paths = [include_paths]
-        for include_path in include_paths:
-            dir_prefix = os.path.dirname(file_path)
-            include_path = os.path.join(dir_prefix, include_path)
-            data.update(load_config_file(include_path))
-        for key, value in data.items():
-            data[key] = resolve_includes(file_path, value)
-    return data
+log = logging.getLogger(__name__)
 
 
 def bulk_load(api, mapping_file):
     data = load_config_file(mapping_file)
     for foreign_id, config in data.items():
         collection_id = load_collection(api, foreign_id, config)
+        log.info("Bulk mapping collection ID: %s", collection_id)
         api.map_collection(collection_id, data)
