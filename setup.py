9fca7d5fc4dd9cb815e4321c1b7529d830a452bd
Friedrich Lindenberg <friedrich@pudo.org>
2018-08-23 13:04:26 +0200

    Bump version: 0.6.10 → 0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alephclient',
-    version='0.6.10',
+    version='0.7.0',
     description='Command-line client for Aleph API',
     author='Journalism Development Network',
     author_email='data@occrp.org',
