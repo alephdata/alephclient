a4b3ca2cc449464e2ea27a9557bd584be50463ca
Friedrich Lindenberg <friedrich@pudo.org>
2018-12-28 13:48:40 +0100

    Bump version: 0.8.4 → 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alephclient',
-    version='0.8.4',
+    version='0.9.0',
     description='Command-line client for Aleph API',
     author='Organized Crime and Corruption Reporting Project',
     author_email='data@occrp.org',
