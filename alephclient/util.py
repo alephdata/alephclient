de77ba4841d3c73ccbee504be04308bf83b0ef8d
Friedrich Lindenberg <friedrich@pudo.org>
2019-04-16 22:48:50 +0200

    Remove leftover

--- alephclient/util.py
+++ alephclient/util.py
@@ -14,18 +14,6 @@ except ImportError:
 log = logging.getLogger(__name__)
 
 
-def read_json_stream(stream):
-    count = 0
-    while True:
-        line = stream.readline()
-        if not line:
-            return
-        count += 1
-        if count % 1000 == 0:
-            log.info("Bulk load entities: %s...", count)
-        yield json.loads(line)
-
-
 def backoff(err, failures):
     """Implement a random, growing delay between external service retries."""
     sleep = max(1, failures) * random.random()
