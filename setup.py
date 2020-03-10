bebc060ba56bc1e03664eae318d53cf1d90428d7
Friedrich Lindenberg <friedrich@pudo.org>
2020-03-09 23:04:57 -0500

    Bump version: 1.2.3 → 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='alephclient',
-    version='1.2.3',
+    version='1.2.4',
     description='Command-line client for Aleph API',
     long_description=long_description,
     long_description_content_type='text/markdown',
