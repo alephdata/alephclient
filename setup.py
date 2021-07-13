79c19212b753512fdd7ccd3e4ee3ca66b1238ac5
Steve Haffenden <steve.haffenden@gmail.com>
2021-07-13 09:33:34 +0200

    Revert "Bump version: 2.3.0 → 2.3.1"
    This reverts commit 113e0716e04b5627dd094bd9b9c484aaa77713f3.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="alephclient",
-    version="2.3.1",
+    version="2.3.0",
     description="Command-line client for Aleph API",
     long_description=long_description,
     long_description_content_type="text/markdown",
