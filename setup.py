6bdacec39eb9f4a02330a242f62143dde40c303e
Christian Stefanescu <christian.stefanescu@occrp.org>
2024-08-01 15:19:16 +0200

    Bump version: 2.4.1 → 2.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="alephclient",
-    version="2.4.1",
+    version="2.5.0",
     description="Command-line client for Aleph API",
     long_description=long_description,
     long_description_content_type="text/markdown",
